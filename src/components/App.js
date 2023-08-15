5ddc8b1f7654200b96d461194eed425fb7732643
VipulTrimbake610 <vipultrimbake610@gmail.com>
2023-08-15 13:50:07 +0530

    submitting code-Tue Aug 15 2023 13:50:03 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [option, setOption] = useState("");
+  function handleClick(e){
+    setOption(e.target.innerText);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="parent"> 
+            <h2>Parent Component</h2>
+            <div>
+              <h3>Child Component 1</h3>
+              <button onClick={handleClick}>Option 1</button>
+            </div>
+            <div>
+              <h3>Child Component 2</h3>
+              <button onClick={handleClick}>Option 2</button>
+            </div>
+            Selected Option: {option}
+        </div>
     </div>
   )
 }
